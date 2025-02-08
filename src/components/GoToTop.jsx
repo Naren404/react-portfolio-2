858c546aec052c552c857cc1b9d4b2774c3a3c09
Naren404 <narendrasunar47@gmail.com>
2025-02-08 13:41:52 +1100

    breaking down into components

---
+++ src/components/GoToTop.jsx
@@ -0,0 +1,9 @@
+const GoToTop = () => {
+  return ( 
+    <div className="go-to-top">
+      <a href="#home"><i className="fa-solid fa-angle-up"></i></a>
+    </div>
+   );
+}
+ 
+export default GoToTop;
